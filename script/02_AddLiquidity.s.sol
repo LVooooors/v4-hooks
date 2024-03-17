4804a6bd78f404167f57372345dfaea6e77ffd0c
Orest Tarasiuk <830847+OrestTa@users.noreply.github.com>
2024-03-17 00:07:13 +0000

    add liquidity and set addresses

--- script/02_AddLiquidity.s.sol
+++ script/02_AddLiquidity.s.sol
@@ -14,17 +14,17 @@ import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 contract AddLiquidityScript is Script {
     using CurrencyLibrary for Currency;
 
-    address constant GOERLI_POOLMANAGER = address(0x3A9D48AB9751398BbFa63ad67599Bb04e4BdF98b); // pool manager deployed to GOERLI
-    address constant MUNI_ADDRESS = address(0xbD97BF168FA913607b996fab823F88610DCF7737); // mUNI deployed to GOERLI -- insert your own contract address here
-    address constant MUSDC_ADDRESS = address(0xa468864e673a807572598AB6208E49323484c6bF); // mUSDC deployed to GOERLI -- insert your own contract address here
-    address constant HOOK_ADDRESS = address(0x3CA2cD9f71104a6e1b67822454c725FcaeE35fF6); // address of the hook contract deployed to goerli -- you can use this hook address or deploy your own!
+    address constant POOL_MANAGER = address(0xE5dF461803a59292c6c03978c17857479c40bc46);
+    address constant TOKEN1_ADDRESS = address(0xA47757c742f4177dE4eEA192380127F8B62455F5);
+    address constant TOKEN2_ADDRESS = address(0xFDA93151f6146f763D3A80Ddb4C5C7B268469465);
+    address constant HOOK_ADDRESS = address(0x0301cF874CDB90ea311354eC6518aeF36F00C5FE);
 
-    PoolModifyLiquidityTest lpRouter = PoolModifyLiquidityTest(address(0x83feDBeD11B3667f40263a88e8435fca51A03F8C));
+    PoolModifyLiquidityTest lpRouter = PoolModifyLiquidityTest(address(0xd962b16F4ec712D705106674E944B04614F077be));
 
     function run() external {
         // sort the tokens!
-        address token0 = uint160(MUSDC_ADDRESS) < uint160(MUNI_ADDRESS) ? MUSDC_ADDRESS : MUNI_ADDRESS;
-        address token1 = uint160(MUSDC_ADDRESS) < uint160(MUNI_ADDRESS) ? MUNI_ADDRESS : MUSDC_ADDRESS;
+        address token0 = uint160(TOKEN2_ADDRESS) < uint160(TOKEN1_ADDRESS) ? TOKEN2_ADDRESS : TOKEN1_ADDRESS;
+        address token1 = uint160(TOKEN2_ADDRESS) < uint160(TOKEN1_ADDRESS) ? TOKEN1_ADDRESS : TOKEN2_ADDRESS;
         uint24 swapFee = 4000; // 0.40% fee tier
         int24 tickSpacing = 10;
 
@@ -38,9 +38,9 @@ contract AddLiquidityScript is Script {
 
         // approve tokens to the LP Router
         vm.broadcast();
-        IERC20(token0).approve(address(lpRouter), 1000e18);
+        IERC20(token0).approve(address(lpRouter), 2e18);
         vm.broadcast();
-        IERC20(token1).approve(address(lpRouter), 1000e18);
+        IERC20(token1).approve(address(lpRouter), 2e18);
 
         // optionally specify hookData if the hook depends on arbitrary data for liquidity modification
         bytes memory hookData = new bytes(0);
@@ -51,8 +51,8 @@ contract AddLiquidityScript is Script {
         console.log("Pool ID Below");
         console.logBytes32(bytes32(idBytes));
 
-        // Provide 10_000e18 worth of liquidity on the range of [-600, 600]
+        // Provide liquidity on the range of [-600, 600]
         vm.broadcast();
-        lpRouter.modifyLiquidity(pool, IPoolManager.ModifyLiquidityParams(-600, 600, 10_000e18), hookData);
+        lpRouter.modifyLiquidity(pool, IPoolManager.ModifyLiquidityParams(-600, 600, 1e18), hookData);
     }
 }
